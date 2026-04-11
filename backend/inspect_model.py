3ebe7856bbdad695200211946a1108c40325d754
CHACHA0044 <demblapranav@gmail.com>
2026-04-11 07:31:08 +0530

    chore: sync metadata and fix embedded repo

--- backend/inspect_model.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForImageClassification
-import os
-os.environ["HF_HOME"] = "d:/rep/tai/.cache/huggingface"
-model = AutoModelForImageClassification.from_pretrained("prithivMLmods/Deep-Fake-Detector-v2-Model")
-print(model.config.id2label)
